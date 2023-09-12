f14cf1eaa3a103a183ef54b6fdca962a283fd7d3
Jefferson <jefferson.brandao@g.globo>
2023-09-11 22:19:17 -0300

    adicionando hover card na temperatura

--- src/components/temperature-selector.tsx
+++ src/components/temperature-selector.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Label } from "./ui/label";
 import { Slider } from "./ui/slider";
 import { SliderProps } from "@radix-ui/react-slider";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 
 interface TemperatureSelectorProps {
   defaultValue: SliderProps["defaultValue"];
@@ -10,26 +11,36 @@ interface TemperatureSelectorProps {
 const TemperatureSelector = ({ defaultValue }: TemperatureSelectorProps) => {
   const [value, setValue] = React.useState(defaultValue);
   return (
-    <div className="space-y-4">
-      <div className="flex items-center justify-between">
-        <Label>Temperatura</Label>
-        <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
-          {value}
-        </span>
-      </div>
-
-      <Slider
-        min={0}
-        max={1}
-        step={0.1}
-        defaultValue={value}
-        onValueChange={setValue}
-      />
-
-      <span className="block text-xs text-muted-foreground italic leading-relaxed">
-        Valores mais altos tendem deixar o resultado mais criativos e com
-        possibilidade de mais erros.
-      </span>
+    <div className="grid gap-2 pt-1">
+      <HoverCard openDelay={200}>
+        <HoverCardTrigger asChild>
+          <div className="grid gap-4">
+            <div className="flex items-center justify-between">
+              <Label htmlFor="temperature">Temperatura</Label>
+              <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+                {value}
+              </span>
+            </div>
+            <Slider
+              min={0}
+              max={1}
+              step={0.1}
+              defaultValue={value}
+              onValueChange={setValue}
+            />
+          </div>
+        </HoverCardTrigger>
+        <HoverCardContent
+          align="start"
+          className="w-[260px] text-sm"
+          side="left"
+        >
+          <span className="block text-xs text-muted-foreground italic leading-relaxed">
+            Valores mais altos tendem deixar o resultado mais criativos e com
+            possibilidade de mais erros.
+          </span>
+        </HoverCardContent>
+      </HoverCard>
     </div>
   );
 };
